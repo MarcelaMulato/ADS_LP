f94c704067a5a971580d2b3ab4b33527390055f8
Marcela Mulato <marcela.mulato7@gmail.com>
2025-04-20 17:27:27 -0300

    Update Main.java

--- Listas de Exercícios/LP_Lista_02/ex19/src/Main.java
+++ Listas de Exercícios/LP_Lista_02/ex19/src/Main.java
@@ -18,8 +18,8 @@ public class Main {
         delta = (b * b) + (-4 * (a * c));
 
         if (delta >= 0) {
-            x1 = ((-(b) + Math.sqrt(delta)) / 2 * a);
-            x2 = ((-(b) - Math.sqrt(delta)) / 2 * a);
+            x1 = (-(b) + Math.sqrt(delta)) / (2 * a);
+            x2 = (-(b) - Math.sqrt(delta)) / (2 * a);
 
             System.out.println("x1 = " + x1);
             System.out.println("x2 = " + x2);
